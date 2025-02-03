68dc5f1cdf6f087b819e5c70bf32fca684d178a7
vinicciusnev <xxnashflows@hotmail.com>
2025-02-03 10:10:49 -0300

    feat: Adicionando Exception personalizada de acesso negado

--- src/main/java/com/erpoticastec/backenderp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/erpoticastec/backenderp/exceptions/GlobalExceptionHandler.java
@@ -6,12 +6,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<?> handleInvalidCredentialsException(InvalidCredentialsException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        return new ResponseEntity<>("Acesso negado", HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(Exception.class)
