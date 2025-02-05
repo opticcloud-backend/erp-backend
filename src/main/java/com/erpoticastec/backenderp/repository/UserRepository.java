e669928e9bffc5cf7b3f35170fb0af2dd1ad699a
vinicciusnev <xxnashflows@hotmail.com>
2025-02-05 09:59:24 -0300

    feat: Adicionando findOticaIdsByUsuario

--- src/main/java/com/erpoticastec/backenderp/repository/UserRepository.java
+++ src/main/java/com/erpoticastec/backenderp/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.erpoticastec.backenderp.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.erpoticastec.backenderp.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query("SELECT uo.otica.id FROM UsuarioOtica uo WHERE uo.usuario.id = :usuarioId")
+    List<Long> findOticaIdsByUsuario(@Param("usuarioId") Long usuarioId);
 }
