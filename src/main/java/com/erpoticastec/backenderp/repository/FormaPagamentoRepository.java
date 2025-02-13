9660e067432809b27a3ed51920c2a9207e65847a
vinicciusnev <xxnashflows@hotmail.com>
2025-02-13 20:55:20 -0300

    feat: Adicionando metodo para buscar a forma de pagamento pela descricao

--- src/main/java/com/erpoticastec/backenderp/repository/FormaPagamentoRepository.java
+++ src/main/java/com/erpoticastec/backenderp/repository/FormaPagamentoRepository.java
@@ -1,7 +1,12 @@
 package com.erpoticastec.backenderp.repository;
 
 import com.erpoticastec.backenderp.model.FormaPagamento;
+import com.erpoticastec.backenderp.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+    Optional<FormaPagamento> findByMetodo(String metodo);
 }
