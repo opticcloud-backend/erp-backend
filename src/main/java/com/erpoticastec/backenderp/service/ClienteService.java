d77d717cc3bf6453c297090345aec1be121c2bc1
vinicciusnev <xxnashflows@hotmail.com>
2025-02-25 22:01:13 -0300

    fix: Melhorando logs do metodo cadastrarCliente

--- src/main/java/com/erpoticastec/backenderp/service/ClienteService.java
+++ src/main/java/com/erpoticastec/backenderp/service/ClienteService.java
@@ -36,16 +36,20 @@ public class ClienteService {
         cliente.setDocumento(clienteRequestDTO.documento());
 
         TipoPessoa tipoPessoa = tipoPessoaRepository.findByDescricao(clienteRequestDTO.tipoCliente())
-                .orElseThrow(() -> new EntityNotFoundException("Tipo de pessoa nao encontrado"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Tipo de pessoa '%s' nao encontrado", clienteRequestDTO.tipoCliente())));
         cliente.setTipoPessoa(tipoPessoa);
 
         Usuario usuarioCadastro = usuarioRepository.findByEmail(clienteRequestDTO.emailUsuarioCadastro())
-                .orElseThrow(() -> new EntityNotFoundException("Usuario ao encontrado"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Usuario com e-mail '%s' nao encontrado", clienteRequestDTO.emailUsuarioCadastro())));
         cliente.setUsuario(usuarioCadastro);
 
         Otica otica = oticaRepository.findById(clienteRequestDTO.oticaId())
-                .orElseThrow(() -> new EntityNotFoundException("Otica nao encontrada"));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Otica com ID '%d' nao encontrada", clienteRequestDTO.oticaId())));
         cliente.setOtica(otica);
+
 
         cliente.setTelefone(clienteRequestDTO.telefone());
         cliente.setEnderecoLogradouro(clienteRequestDTO.enderecoLogradouro());
@@ -55,7 +59,7 @@ public class ClienteService {
         cliente.setEnderecoCidade(clienteRequestDTO.enderecoCidade());
         cliente.setEnderecoEstado(clienteRequestDTO.enderecoEstado());
         cliente.setEnderecoCep(clienteRequestDTO.enderecoCep());
-        cliente.setDataNascimento(((clienteRequestDTO.dataNascimento() != null) || clienteRequestDTO.dataNascimento().toString().isEmpty()) ? clienteRequestDTO.dataNascimento() : null);
+        cliente.setDataNascimento(clienteRequestDTO.dataNascimento());
         cliente.setRazaoSocial(clienteRequestDTO.razaoSocial());
         cliente.setNomeFantasia(clienteRequestDTO.nomeFantasia());
         cliente.setInscricaoEstadual(clienteRequestDTO.inscricaoEstadual());
@@ -63,7 +67,8 @@ public class ClienteService {
         cliente.setLimiteCredito(clienteRequestDTO.limiteCredito());
 
         FormaPagamento formaPagamento = formaPagamentoRepository.findByMetodo(clienteRequestDTO.metodoPagamentoPreferido())
-                .orElseThrow(() -> new EntityNotFoundException("Forma de pagamento nao encontrada"));
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Forma de pagamento %s nao encontrada", clienteRequestDTO.metodoPagamentoPreferido())));
+
         cliente.setFormaPagamentoPreferida(formaPagamento);
 
         cliente.setIndicadorCliente(clienteRequestDTO.indicadorCliente());
@@ -129,7 +134,8 @@ public class ClienteService {
 
         if (pessoaUpdateDTO.tipoCliente() != null) {
             TipoPessoa tipoPessoa = tipoPessoaRepository.findByDescricao(pessoaUpdateDTO.tipoCliente())
-                    .orElseThrow(() -> new EntityNotFoundException("Tipo de Pessoa não encontrado"));
+                    .orElseThrow(() -> new EntityNotFoundException(
+                            String.format("Tipo de pessoa '%s' nao encontrado", pessoaUpdateDTO.tipoCliente())));
             existingClienteFornecedor.setTipoPessoa(tipoPessoa);
         }
 
@@ -159,7 +165,7 @@ public class ClienteService {
 
         if (pessoaUpdateDTO.metodoPagamentoPreferido() != null) {
             FormaPagamento formaPagamento = formaPagamentoRepository.findByMetodo(pessoaUpdateDTO.metodoPagamentoPreferido())
-                    .orElseThrow(() -> new EntityNotFoundException("Forma de pagamento nao encontrado"));
+                    .orElseThrow(() -> new EntityNotFoundException(String.format("Forma de pagamento %s nao encontrada", pessoaUpdateDTO.metodoPagamentoPreferido())));
 
             existingClienteFornecedor.setFormaPagamentoPreferida(formaPagamento);
         }
